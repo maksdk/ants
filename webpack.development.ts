0429745badbd58c70f170ace31b585f7b09383be
maksdk <havryshchenko@gmail.com>
2020-08-26 21:19:26 +0300

    Remove unused deps / Remove hot reload

--- webpack.development.ts
+++ webpack.development.ts
@@ -1,66 +1,74 @@
-import * as webpack from "webpack";
-import * as path from "path";
+import * as webpack from 'webpack';
+import * as path from 'path';
 
-import ForkTsCheckerNotifierWebpackPlugin from "fork-ts-checker-notifier-webpack-plugin";
-import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
+import ForkTsCheckerNotifierWebpackPlugin from 'fork-ts-checker-notifier-webpack-plugin';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
-module.exports = (env: { mode: "development" | "production" }) => {
+module.exports = (env: { mode: 'development' | 'production' }) => {
     /** @type {import('webpack').Configuration} */
     const devConfig = {
         mode: env.mode,
 
-        devtool: "cheap-module-eval-source-map",
+        devtool: 'cheap-module-eval-source-map',
+
+        devServer: {
+            hot: false,
+            liveReload: false,
+            open: true
+        },
+
+        watch: true,
 
         module: {
             rules: [
                 {
-                    enforce: "pre",
+                    enforce: 'pre',
                     test: /\.(js|jsx|ts|tsx)$/,
                     exclude: /node_modules/,
-                    loader: "eslint-loader",
+                    loader: 'eslint-loader',
                     options: {
-                        fix: true,
-                    },
+                        fix: true
+                    }
                 },
                 {
                     test: /\.tsx?$/,
-                    loader: "ts-loader",
+                    loader: 'ts-loader',
                     exclude: /node_modules/,
                     options: {
-                        transpileOnly: true,
-                    },
-                },
-            ],
+                        transpileOnly: true
+                    }
+                }
+            ]
         },
 
         output: {
-            path: path.resolve(__dirname, "dist"),
-            filename: "game.js",
-            chunkFilename: "game-library.js",
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'game.js',
+            chunkFilename: 'game-library.js'
         },
 
         plugins: [
             new ForkTsCheckerWebpackPlugin({
                 eslint: {
-                    files: "./src/**/*.{ts,tsx,js,jsx}",
-                },
+                    files: './src/**/*.{ts,tsx,js,jsx}'
+                }
             }),
 
             new ForkTsCheckerNotifierWebpackPlugin({
-                skipSuccessful: true,
+                skipSuccessful: true
             }),
 
             new MiniCssExtractPlugin({
-                filename: "[name].css",
+                filename: '[name].css'
             }),
 
             new webpack.DefinePlugin({
                 PRODUCTION: JSON.stringify(false),
-                VERSION: JSON.stringify("3.0.0"), // TODO Update from package.json
-            }),
-        ],
+                VERSION: JSON.stringify('3.0.0') // TODO Update from package.json
+            })
+        ]
     };
 
     return devConfig;
