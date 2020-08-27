f654c62df2b8f849ddd123240c3417bf24c7f252
maksdk <havryshchenko@gmail.com>
2020-08-27 08:46:36 +0300

    Clear lint errors

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,24 +1,21 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
+import * as path from 'path';
 
-import * as path from "path";
+import { merge } from 'webpack-merge';
+import { Configuration } from 'webpack';
 
-import { merge } from "webpack-merge";
-import { Configuration } from "webpack";
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+import LodashModuleReplacementPlugin from 'lodash-webpack-plugin';
 
-// plugins
-import HtmlWebpackPlugin from "html-webpack-plugin";
-import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import CopyPlugin from "copy-webpack-plugin";
-import LodashModuleReplacementPlugin from "lodash-webpack-plugin";
-
-module.exports = (env: { mode: "development" | "production" }) => {
-    const developmentMode = env.mode === "development";
+module.exports = (env: { mode: 'development' | 'production' }) => {
+    const developmentMode = env.mode === 'development';
 
     const config: Configuration = {
-        entry: "./src/index.ts",
+        entry: './src/index.ts',
 
         resolve: {
-            extensions: [".ts", ".tsx", ".js", ".json"],
+            extensions: ['.ts', '.tsx', '.js', '.json']
         },
 
         module: {
@@ -29,47 +26,47 @@ module.exports = (env: { mode: "development" | "production" }) => {
                         {
                             loader: MiniCssExtractPlugin.loader,
                             options: {
-                                hmr: developmentMode,
-                            },
+                                hmr: developmentMode
+                            }
                         },
-                        "css-loader",
-                    ],
-                },
-            ],
+                        'css-loader'
+                    ]
+                }
+            ]
         },
         optimization: {
             splitChunks: {
-                chunks: "all",
-            },
+                chunks: 'all'
+            }
         },
 
         plugins: [
             new LodashModuleReplacementPlugin(),
             new HtmlWebpackPlugin({
-                title: "TS PIXI BOILERPLATE",
+                title: 'TS PIXI BOILERPLATE',
                 meta: {
-                    viewport: "initial-scale = 1.0, maximum-scale = 1.0, user-scalable=no",
-                    "Content-Type": { "http-equiv": "Content-Type", content: "text/html; charset=utf-8" },
+                    viewport: 'initial-scale = 1.0, maximum-scale = 1.0, user-scalable=no',
+                    'Content-Type': { 'http-equiv': 'Content-Type', content: 'text/html; charset=utf-8' }
                 },
-                filename: "index.html",
-                template: path.resolve(__dirname, "./index.html"),
+                filename: 'index.html',
+                template: path.resolve(__dirname, './index.html')
             }),
             new CopyPlugin({
                 patterns: [
                     {
-                        from: "assets/**",
+                        from: 'assets/**',
 
                         // if there are nested subdirectories , keep the hierarchy
                         transformPath(targetPath, absolutePath) {
-                            const assetsPath = path.resolve(__dirname, "assets");
+                            const assetsPath = path.resolve(__dirname, 'assets');
                             const endpPath = absolutePath.slice(assetsPath.length);
 
                             return Promise.resolve(`assets/${endpPath}`);
-                        },
-                    },
-                ],
-            }),
-        ],
+                        }
+                    }
+                ]
+            })
+        ]
     };
     const envConfig = require(path.resolve(__dirname, `./webpack.${env.mode}.ts`))(env);
 
