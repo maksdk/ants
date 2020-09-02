67eaf8ade24bd0c53550a5e82224ed8becf31e0d
maksdk <havryshchenko@gmail.com>
2020-09-02 21:33:26 +0300

    Correct entry point

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -1,80 +1,51 @@
-import { Model } from './Model';
-import { Scene } from './Scene';
-import { Ticker } from '../../libs/Ticker';
-import { ResizeManager } from '../../libs/ResizeManager';
-import { UIBuilder } from '../../libs/UIBuilder';
-
 import TWEEN from '@tweenjs/tween.js';
+import { World } from 'ecsy';
+import { Model } from '@game/Model';
+import { Scene } from '@game/Scene';
+import { Ticker } from '@libs/Ticker';
+import { ResizeManager } from '@libs/ResizeManager';
+import { AssetsPreloader } from '@libs/AssetsPreloader';
+import { GameFSM } from '@fsm/GameFSM';
+import assets from '@assets';
 
 import Data = Core.Data;
 
 export class Game {
-    private app: PIXI.Application;
     public model: Model;
+    public loader: AssetsPreloader;
     public ticker: Ticker;
     public scene: Scene;
     public resizeManager: ResizeManager;
+    public fsm: GameFSM;
+    public world: World;
+    private app: PIXI.Application;
 
     constructor(app: PIXI.Application) {
         this.app = app;
         this.model = new Model();
-        this.ticker = new Ticker(app);
-        this.scene = new Scene({ name: 'MainScene', zIndex: 0, config: {} });
-        this.resizeManager = new ResizeManager(app, { width: 900, height: 900 });
+        this.ticker = new Ticker(this.app);
+        this.scene = new Scene({ name: 'RootScene', zIndex: 0, config: {} });
+        this.resizeManager = new ResizeManager(this.app, { width: 900, height: 900 });
+        this.loader = new AssetsPreloader(this.app, assets);
+        this.world = new World();
+        this.fsm = new GameFSM(this);
+    }
 
+    public start(): void {
         this.app.stage.addChild(this.scene);
         this.ticker.add('Game', this.update.bind(this));
         this.resizeManager.add('Game', this.onResize.bind(this));
+        this.resizeManager.resize();
+        this.fsm.start();
     }
 
-    run(): void {
-        const ui = this.scene.getLayer('UI');
-
-        const kenny = UIBuilder.createSprite({
-            name: 'kenny',
-            textureName: 'kenny',
-            modifiers: {
-                anchor: { x: 0.5, y: 0.5 },
-                scale: { x: 0.5, y: 0.5 }
-            }
-        });
-
-        new TWEEN.Tween({ angle: 1 })
-            .to({ angle: 360 }, 3000)
-            .onUpdate(({ angle }) => {
-                kenny.scale.x = Math.cos(angle / 20) * 2;
-                kenny.scale.y = Math.sin(angle / 25) * 2;
-                kenny.angle = angle;
-            })
-            .repeat(Infinity)
-            .yoyo(true)
-            .start(0);
-
-        const title = UIBuilder.createText({
-            name: 'title',
-            text: 'They killed kenny!!!',
-            style: {
-                fontSize: 80,
-                fill: 0xffffff,
-                fontFamily: 'Coneria',
-                padding: 30
-            }
-        });
-
-        ui.addChild(kenny, title);
-    }
-
-    public update(dt: number, totalTime: number): void {
+    private update(dt: number, totalTime: number): void {
         TWEEN.update(totalTime);
-        /*do nothing here for now*/
+        this.world.execute(dt);
     }
 
     private onResize(sizes: Data.IResize): void {
-        /*do nothing here for now*/
         const { game } = sizes;
         this.scene.position.set(game.width / 2, game.height / 2);
-
-        const title = this.scene.getLayer('UI').getChildByName('title');
-        title.position.set(-game.width / 2, -game.height / 2);
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,12 @@
-import { AssetsPreloader } from '../libs/AssetsPreloader';
 import { Application } from 'pixi.js';
-import assets from '../assets';
+import { GameConfig } from '@config/GameConfig';
+import { Game } from '@game/Game';
 
-import { GameConfig } from './configs/GameConfig';
-import { Game } from './game/Game';
 const gameConfig = new GameConfig();
 const { application, gameCanvasId } = gameConfig;
 
 application.view = document.body.querySelector(gameCanvasId) as HTMLCanvasElement;
-const app: PIXI.Application = new Application(application);
-const game: Game = new Game(app);
 
-const onAssetsLoaded = () => {
-    game.run();
-    game.resizeManager.resize();
-};
-
-const onAssetsLoadProgress = (percentProgress: number) => {
-    console.log('Progress: ', percentProgress);
-};
-
-const assetsPreloader = new AssetsPreloader(app, assets);
-assetsPreloader.load(onAssetsLoaded, onAssetsLoadProgress);
+const app = new Application(application);
+const game = new Game(app);
+game.start();
