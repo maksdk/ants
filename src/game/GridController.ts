1e95aedd790eba71f22116e4ab585f3f1ce2590a
maksdk <havryshchenko@gmail.com>
2020-09-04 19:09:23 +0300

    Add mechanic to find deep neighbors

--- src/game/GridController.ts
+++ src/game/GridController.ts
@@ -19,4 +19,41 @@ export class GridController {
     public findSmoothPath(fx: number, fy: number, tx: number, ty: number): number[][] {
         return Util.smoothenPath(this.grid, this.findPath(fx, fy, tx, ty));
     }
+
+    public getFreeNeighbors(fx: number, fy: number): { x: number; y: number }[] | never {
+        if (!this.grid.isInside(fx, fy)) {
+            throw new Error(`Points: "x: ${fx}" and "y: ${fy}" is not inside of Grid !!!`);
+        }
+
+        const node = { x: fx, y: fy, walkable: this.grid.isWalkableAt(fx, fy) };
+
+        const neighbors = this.grid.getNeighbors(node, DiagonalMovement.OnlyWhenNoObstacles);
+
+        return neighbors.filter(({ walkable }) => !!walkable).map(({ x, y }) => ({ x, y }));
+    }
+
+    public getFreeDeepNeighbors(fx: number, fy: number, r: number): { x: number; y: number }[] | never {
+        if (!this.grid.isInside(fx, fy)) {
+            throw new Error(`Points: "x: ${fx}" and "y: ${fy}" is not inside of Grid !!!`);
+        }
+
+        const x0 = fx - r;
+        const y0 = fy - r;
+        const d = r * 2;
+        const result = [];
+
+        for (let y = y0; y < y0 + d; y += 1) {
+            for (let x = x0; x < x0 + d; x += 1) {
+                if (!this.grid.isInside(x, y)) continue;
+
+                const isWalkable = this.grid.isWalkableAt(x, y);
+
+                if (isWalkable) {
+                    result.push({ x, y });
+                }
+            }
+        }
+
+        return result;
+    }
 }
